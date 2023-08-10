58bb4f7bbccbf58ce027c62d99e2c8f7778ed25e
JOÃO VICTOR DO REGO SOUZA ÁLVARES <joao.alvares@a.ucb.br>
2023-08-10 10:35:30 -0300

    Tarefa corrigida

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,6 +1,29 @@
 let a = 5
 let b = 10
 
-let resultado = a+b
+let soma = a+b
 
-console.log(`O resultado da soma entre 'a' e 'b' é ${resultado}`) // template string
+console.log(`O resultado da soma entre 'a' e 'b' é ${soma}`) // template string
+
+let c = 2
+let d = 4
+
+let multiplicacao = c*d
+
+console.log(`O resultado da multiplicação entre 'a' e 'b' é ${multiplicacao}`) 
+
+let e = 10
+let f = 2
+
+let divisao = e/f
+
+console.log(`O resultado da divisão entre 'a' e 'b' é ${divisao}`) 
+
+
+let g = 10
+let h = 5
+
+let subtracao = 10-5
+
+console.log(`O resultado da subtração entre 'a' e 'b' é ${subtracao}`) 
+
