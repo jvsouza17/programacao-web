108ec1b53b99b2b7a3b6dcb1292a6321411ba1a9
JOÃO VICTOR DO REGO SOUZA ÁLVARES <joao.alvares@a.ucb.br>
2023-08-10 10:42:24 -0300

    Tarefa corrigida

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -3,21 +3,21 @@ let b = 10
 
 let soma = a+b
 
-console.log(`O resultado da soma entre 'a' e 'b' é ${soma}`) // template string
+console.log(`O resultado da soma entre ${a} e ${b} é ${soma}`) // template string
 
 let c = 2
 let d = 4
 
 let multiplicacao = c*d
 
-console.log(`O resultado da multiplicação entre 'a' e 'b' é ${multiplicacao}`) 
+console.log(`O resultado da multiplicação entre ${c} e ${d} é ${multiplicacao}`) 
 
 let e = 10
 let f = 2
 
 let divisao = e/f
 
-console.log(`O resultado da divisão entre 'a' e 'b' é ${divisao}`) 
+console.log(`O resultado da divisão entre ${e} e ${f} é ${divisao}`) 
 
 
 let g = 10
@@ -25,5 +25,5 @@ let h = 5
 
 let subtracao = 10-5
 
-console.log(`O resultado da subtração entre 'a' e 'b' é ${subtracao}`) 
+console.log(`O resultado da subtração entre ${g} e ${h} é ${subtracao}`) 
 
