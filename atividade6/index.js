f7df82444a1667d7e116b60ccfde0127a6415c18
João Victor <joao.alvares@a.ucb.br>
2023-10-05 10:29:45 -0300

    Corrigir atividade6

--- atividade6/index.js
+++ atividade6/index.js
@@ -10,14 +10,17 @@ app.use(express.urlencoded({ extended: false })); // necessário para reconhecer
 // app.use(express.text());
 // app.use(express.json());
 
-app.get('/forms', (req, res) => { //por meio do método get, ao acessar a rota /forms iremos preencher o formulário presente no HTML 
-    res.render('forms')            // ao apertar no botão enviar do formulário seremos direcionados a rota /submit (action=/submit)
+app.get('/', (req, res) => { // por meio do método get, ao acessar a rota /forms iremos preencher o formulário presente no HTML 
+    res.render('forms.html') // renderizar a página html    
 });
-
-app.post('/submit', (req, res) => { // aqui, a rota submit é preparada para ser acessada durante o post
-    res.send(`Nome: ${req.body.nome}\n`+  
-            `Email: ${req.body.email}\n`+ 
-            `Telefone: ${req.body.telefone}\n`) // ela irá trazer as informações passadas no formulário da rota /forms
+                                    // ao apertar no botão enviar do formulário seremos direcionados a rota /submit (action=/submit)
+app.post('/dados', (req, res) => { // aqui, a rota submit é preparada para ser acessada durante o post
+    let formulario = {
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone
+    }
+    res.render('dados.html', {formulario}) // ela irá trazer as informações passadas no formulário da rota /forms
 })
 
 const PORT = 8080;
